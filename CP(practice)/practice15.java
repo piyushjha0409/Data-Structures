64f72404a86c88f7da1af1d545db7d220afc7bb0
Piyush Jha <73685420+piyushjha0409@users.noreply.github.com>
2022-02-27 13:23:28 +0530

    changes applied

--- CP(practice)/practice15.java
+++ CP(practice)/practice15.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class practice15 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
         
+        int arr[] = new int [];
+    }
+
+    static void removeDuplicates(int arr[]. int size){
+
     }
 }
