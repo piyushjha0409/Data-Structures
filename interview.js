02ad3fe89ea36eac22346edfc48905d82393ff58
Piyush <peeyush0409@gmail.com>
2022-10-06 17:58:55 +0530

    js practice

--- interview.js
+++ interview.js
@@ -1,4 +1,8 @@
-let a = []
-b = a
-console.warn(a == b)
-console.warn(a === b)
+let a = ['20']
+let b = [20]
+console.warn(a[0] == b[0])
+console.warn(a[0] === b[0])  // three equals means it will compare the type as well
+
+let x = [1,3,21,12]
+console.log(...x)
+
